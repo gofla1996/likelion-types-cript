9f3796b02084f9080274873f0b483142f6091075
Hyerim <110741@daum.net>
2025-01-08 16:42:51 +0900

    [실습] express 이용한 데이터 API 및 라우터 실습

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# Learning TypeScript
+# TypeScript 에센셜
 
-TypeScript 프로그래밍을 학습합니다.
+TypeScript 리마인드 학습 및 TypeScript를 활용해 간단한 API를 제공하는 Express 서버 애플리케이션을 구성합니다.
+이 과정을 통해 향후 학습할 React에서 TypeScript 사용이 익숙해질 것을 기대합니다.
 
 ## 타입 에너테이션
 
---
+++ src/middlewares/greetingMessage.ts
@@ -0,0 +1,12 @@
+import type { NextFunction, Request, Response } from "express";
+
+function greetingMessage(request: Request, response: Response, next: NextFunction) {
+  const date = new Date();
+  console.log(`Happy New Year!! 🫰 ${date.getFullYear()}`);
+
+  // ...
+
+  next();
+}
+
+export default greetingMessage;
